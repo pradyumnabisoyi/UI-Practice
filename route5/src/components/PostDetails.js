ea32667c7cc4488713c1751e01414e725341d332
Pradyumna <pradyumn.bisoyi.java@gail.com>
2022-08-18 09:59:45 +0530

    Query param

--- route5/src/components/PostDetails.js
+++ route5/src/components/PostDetails.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const PostDetails = (props) => {
+    console.log(props.location.search.split('=')[1]);
     return (
         <div className='panel panel-danger'>
             <div className="panel-heading">{props.match.params.topic} Page</div>
             <div className="panel-body">In this {props.match.params.topic} tutorial, you will find more than 200 examples. With our online "Try it Yourself" editor, you can edit and test each example yourself!In this {props.match.params.topic} tutorial, you will find more than 200 examples. With our online "Try it Yourself" editor, you can edit and test each example yourself!In this {props.match.params.topic} tutorial, you will find more than 200 examples. With our online "Try it Yourself" editor, you can edit and test each example yourself!In this {props.match.params.topic} tutorial, you will find more than 200 examples. With our online "Try it Yourself" editor, you can edit and test each example yourself!In this HTML tutorial, you will find more than 200 examples. With our online "Try it Yourself" editor, you can edit and test each example yourself!</div>
+            <h3>Page No : {props.location.search.split('=')[1]}</h3>
         </div>
     )
 
